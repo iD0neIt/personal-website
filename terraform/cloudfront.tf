69b955c58a2fe4ad42e0e2affc1525766baad574
Rahul <rahulprat247@gmail.com>
2023-12-23 21:17:03 +0000

    update security policy

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -63,7 +63,7 @@ resource "aws_cloudfront_response_headers_policy" "website" {
             override = true
         }
         content_security_policy {
-            content_security_policy = "default-src 'none'; style-src-elem 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+            content_security_policy = "default-src 'none'; style-src-elem 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; img-src 'self'; script-src 'unsafe-inline'; style-src 'self'; object-src 'none'"
             override = true
         }
     }
